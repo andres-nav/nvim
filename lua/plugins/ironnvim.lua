7084cafffe8f279bba43a8ee2357880421340147
Andres Navarro <contact@andresnav.com>
2023-01-31 19:46:34 +0100

    Added iron.nvim and changed some lsp

---
+++ lua/plugins/ironnvim.lua
@@ -0,0 +1,53 @@
+return {
+  {
+    "hkupty/iron.nvim",
+    config = function()
+      local iron = require("iron.core")
+
+      iron.setup({
+        config = {
+          -- Whether a repl should be discarded or not
+          scratch_repl = true,
+          -- Your repl definitions come here
+          repl_definition = {
+            sh = {
+              -- Can be a table or a function that
+              -- returns a table (see below)
+              command = { "fish" },
+            },
+          },
+          -- How the repl window will be displayed
+          -- See below for more information
+          repl_open_cmd = require("iron.view").split.vertical.botright(0.4),
+        },
+        -- Iron doesn't set keymaps by default anymore.
+        -- You can set them here or manually add keymaps to the functions in iron.core
+        keymaps = {
+          send_motion = "<space>rm",
+          visual_send = "<space>rc",
+          send_file = "<space>rF",
+          send_line = "<space>rl",
+          send_mark = "<space>rm",
+          mark_motion = "<space>mc",
+          mark_visual = "<space>mc",
+          remove_mark = "<space>md",
+          cr = "<space>s<cr>",
+          interrupt = "<space>r<space>",
+          exit = "<space>rq",
+          clear = "<space>rx",
+        },
+        -- If the highlight is on, you can change how it looks
+        -- For the available options, check nvim_set_hl
+        highlight = {
+          italic = true,
+        },
+        ignore_blank_lines = true,
+      })
+
+      vim.keymap.set("n", "<space>rs", "<cmd>IronRepl<cr>")
+      vim.keymap.set("n", "<space>rr", "<cmd>IronRestart<cr>")
+      vim.keymap.set("n", "<space>rf", "<cmd>IronFocus<cr>")
+      vim.keymap.set("n", "<space>rh", "<cmd>IronHide<cr>")
+    end,
+  },
+}
---
+++ lua/plugins/nvim-lspconfig.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        -- pyright will be automatically installed with mason and loaded with lspconfig
+        pyright = {},
+        clangd = {},
+        r_language_server = {},
+      },
+    },
+  },
+}
